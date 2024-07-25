b3eadd3d35136808295d8daeb74afd598f73d28e
Robin Zheng <1487805+zhengr@users.noreply.github.com>
2024-07-25 20:51:07 +0800

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link'
+import { headers } from 'next/headers'
  
-export default function NotFound() {
+export default async function NotFound() {
+  const headersList = headers()
+  const domain = headersList.get('host')
+  const data = await getSiteData(domain)
   return (
     <div>
-      <h2>Not Found</h2>
+      <h2>Not Found: {data.name}</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <p>
+        View <Link href="/blog">all posts</Link>
+      </p>
     </div>
   )
 }
